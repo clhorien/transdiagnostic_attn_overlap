120bff1498302f510dec87bc45e63de849e9a859
Corey Horien <corey.horien@yale.edu>
2025-08-28 20:22:46 -0400

    Initial commit of preprocessing template scripts

---
+++ template_skull_strip.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+for sub in sub1 sub2 sub3
+#copy and paste subjs above
+
+do 
+
+#download optiBET script and changed as needed (referenced here: https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0115551; 
+sh /path/to/optibet/script/optiBET.sh -i ${sub}_*.nii.gz
+
+
+
+done
+
+
+
